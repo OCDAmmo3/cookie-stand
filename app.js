0e24a115b59cab8080990356c8e10cc0751370bc
Jon Struve <ocdammo3@gmail.com>
2019-07-15 16:16:20 -0500

    Finished basic js functions for today

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 "use strict";
 
-var hours = ["6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm","8pm"]
-
 var firstAndPike = {
     minCust: 23,
     maxCust: 65,
     avgCookies: 6.3,
+    hourlyArray: [],
     randCust: function() {
       var numCust = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
       return numCust;
   },
     randCookies: function() {
       for(var i=0; i<hours.length; i++){
-        alki.hourlyArray[i] = Math.floor(this.randCust()*alki.avgCookies);
+        firstAndPike.hourlyArray[i] = Math.floor(this.randCust()*firstAndPike.avgCookies);
       }
     }
 }
@@ -21,13 +20,14 @@ var seaTacAirport = {
     minCust: 3,
     maxCust: 24,
     avgCookies: 1.2,
+    hourlyArray: [],
     randCust: function() {
       var numCust = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
       return numCust;
   },
     randCookies: function() {
       for(var i=0; i<hours.length; i++){
-        alki.hourlyArray[i] = Math.floor(this.randCust()*alki.avgCookies);
+        seaTacAirport.hourlyArray[i] = Math.floor(this.randCust()*seaTacAirport.avgCookies);
       }
     }
 }
@@ -38,13 +38,14 @@ var seattleCenter = {
     minCust: 11,
     maxCust: 38,
     avgCookies: 3.7,
+    hourlyArray: [],
     randCust: function() {
       var numCust = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
       return numCust;
   },
     randCookies: function() {
       for(var i=0; i<hours.length; i++){
-        alki.hourlyArray[i] = Math.floor(this.randCust()*alki.avgCookies);
+        seattleCenter.hourlyArray[i] = Math.floor(this.randCust()*seattleCenter.avgCookies);
       }
     }
 }
@@ -55,13 +56,14 @@ var capitolHill = {
     minCust: 20,
     maxCust: 38,
     avgCookies: 2.3,
+    hourlyArray: [],
     randCust: function() {
       var numCust = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
       return numCust;
   },
     randCookies: function() {
       for(var i=0; i<hours.length; i++){
-        alki.hourlyArray[i] = Math.floor(this.randCust()*alki.avgCookies);
+        capitolHill.hourlyArray[i] = Math.floor(this.randCust()*capitolHill.avgCookies);
       }
     }
 }
@@ -84,9 +86,41 @@ var alki = {
       }
     }
 }
+//
+// console.log(firstAndPike.randCookies());
+// console.log(seaTacAirport.randCookies());
+// console.log(seattleCenter.randCookies());
+// console.log(capitolHill.randCookies());
+// console.log(alki.randCookies());
 
-console.log(firstAndPike.randCookies());
-console.log(seaTacAirport.randCookies());
-console.log(seattleCenter.randCookies());
-console.log(capitolHill.randCookies());
-console.log(alki.randCookies());
+var hours = ["6am","7am","8am","9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm","6pm","7pm","8pm"]
+
+function generate() {
+  firstAndPike.randCust();
+  firstAndPike.randCookies();
+  seaTacAirport.randCust();
+  seaTacAirport.randCookies();
+  seattleCenter.randCust();
+  seattleCenter.randCookies();
+  capitolHill.randCust();
+  capitolHill.randCookies();
+  alki.randCust();
+  alki.randCookies();
+}
+
+function storeData(location,id){
+  var locationList = document.getElementById(id);
+  for(var liIndex = 0; liIndex < location.hourlyArray.length; liIndex++){
+    var li = document.createElement("li");
+    var listString = hours[liIndex] + ": " + location.hourlyArray[liIndex] + " cookies.";
+    li.textContent = listString;
+    locationList.appendChild(li);
+  }
+}
+generate();
+
+storeData(firstAndPike,"pike");
+storeData(seaTacAirport,"seaTac");
+storeData(seattleCenter,"seattle");
+storeData(capitolHill,"capitol");
+storeData(alki,"alki");
