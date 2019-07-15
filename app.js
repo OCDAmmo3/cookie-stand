851338888fb2f78245924b2401b5a53689f5ad4c
Jon Struve <ocdammo3@gmail.com>
2019-07-15 13:48:50 -0500

    Created initial object components

---
+++ app.js
@@ -0,0 +1,31 @@
+var restaurants = [{
+  restaurant: "1st and Pike",
+  minCust: 23,
+  maxCust: 65,
+  avgCookies: 6.3,
+},
+{
+  restaurant: "SeaTac Airport",
+  minCust: 3,
+  maxCust: 24,
+  avgCookies: 1.2
+},
+{
+  restaurant: "Seattle Center",
+  minCust: 11,
+  maxCust: 38,
+  avgCookies: 3.7
+},
+{
+  restaurant: "Capitol Hill",
+  minCust: 20,
+  maxCust: 38,
+  avgCookies: 2.3
+},
+{
+  restaurant: "Alki",
+  minCust: 2,
+  maxCust: 16,
+  avgCookies: 4.6
+}
+]
