39bc5b7b65e5184157c5ee9d9c02718ad95adf48
Jon Struve <ocdammo3@gmail.com>
2019-07-15 14:19:32 -0500

    Added random customer and cookies fuctions

--- app.js
+++ app.js
@@ -1,31 +1,72 @@
-var restaurants = [{
-  restaurant: "1st and Pike",
+"use strict";
+
+var firstAndPike = {
   minCust: 23,
   maxCust: 65,
   avgCookies: 6.3,
+  randCust: function() {
+    var numCust = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+    return numCust;
 },
-{
-  restaurant: "SeaTac Airport",
-  minCust: 3,
-  maxCust: 24,
-  avgCookies: 1.2
-},
-{
-  restaurant: "Seattle Center",
-  minCust: 11,
-  maxCust: 38,
-  avgCookies: 3.7
-},
-{
-  restaurant: "Capitol Hill",
-  minCust: 20,
-  maxCust: 38,
-  avgCookies: 2.3
-},
-{
-  restaurant: "Alki",
-  minCust: 2,
-  maxCust: 16,
-  avgCookies: 4.6
+  randCookies: function() {
+    var numCookies = Math.floor(this.randCust() * this.avgCookies);
+    return numCookies;
+  }
 }
-]
+var seaTacAirport = {
+    minCust: 3,
+    maxCust: 24,
+    avgCookies: 1.2,
+    randCust: function() {
+      var numCust = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+      return numCust;
+  },
+    randCookies: function() {
+      var numCookies = Math.floor(this.randCust() * this.avgCookies);
+      return numCookies;
+    }
+}
+var seattleCenter = {
+    minCust: 11,
+    maxCust: 38,
+    avgCookies: 3.7,
+    randCust: function() {
+      var numCust = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+      return numCust;
+  },
+    randCookies: function() {
+      var numCookies = Math.floor(this.randCust() * this.avgCookies);
+      return numCookies;
+    }
+}
+var capitolHill = {
+    minCust: 20,
+    maxCust: 38,
+    avgCookies: 2.3,
+    randCust: function() {
+      var numCust = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+      return numCust;
+  },
+    randCookies: function() {
+      var numCookies = Math.floor(this.randCust() * this.avgCookies);
+      return numCookies;
+    }
+}
+var alki = {
+    minCust: 2,
+    maxCust: 16,
+    avgCookies: 4.6,
+    randCust: function() {
+      var numCust = Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
+      return numCust;
+  },
+    randCookies: function() {
+      var numCookies = Math.floor(this.randCust() * this.avgCookies);
+      return numCookies;
+    }
+}
+console.log(firstAndPike.randCust());
+console.log(seaTacAirport.randCust());
+console.log(seattleCenter.randCust());
+console.log(capitolHill.randCust());
+console.log(alki.randCust());
